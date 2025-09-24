ba50ebb5a6068303aee64aeadb7401ace129684d
Jelena Uscelemova <jelena.uscelemova@aaro.com>
2025-09-24 17:27:02 +0300

    my 1st commit

---
+++ src/test/java/OpenWebPageSystemTest.java
@@ -0,0 +1,37 @@
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class OpenWebPageSystemTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        // Make sure chromedriver is in your system PATH or specify its location
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testTrevolutionPageTitle() {
+        driver.get("https://dyninno.com/en/division/trevolution/");
+        String title = driver.getTitle();
+        System.out.println("Page title: " + title);
+        assertTrue(title.toLowerCase().contains("trevolution"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
+}
---
+++ src/test/java/calcSystemTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class calcSystemTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
